d0107a082b1a57bcb83e13b491b26af5426071df
Hang Lei <2227874+bebound@users.noreply.github.com>
2022-06-17 17:45:31 +0800

    Fix Fedora36 PYTHONPATH

--- scripts/release/rpm/test_rpm_in_docker.sh
+++ scripts/release/rpm/test_rpm_in_docker.sh
@@ -20,6 +20,11 @@ pip3 install wheel
 pip3 install pytest --prefix /usr/lib64/az
 pip3 install pytest-xdist --prefix /usr/lib64/az
 
+# From Fedora36, when using `pip install --prefix`with root, the package is installed into `{prefix}/local/lib`.
+# Making a copy of the modules to make sure tests can be added later.
+# Ref https://docs.fedoraproject.org/en-US/fedora/latest/release-notes/developers/Development_Python/
+cp -r /usr/lib64/az/lib /usr/lib64/az/local/lib/
+
 find /azure-cli/artifacts/build -name "azure_cli_testsdk*" | xargs pip3 install --prefix /usr/lib64/az --upgrade --ignore-installed
 find /azure-cli/artifacts/build -name "azure_cli_fulltest*" | xargs pip3 install --prefix /usr/lib64/az --upgrade --ignore-installed --no-deps
 
--- scripts/release/rpm/test_rpm_package.py
+++ scripts/release/rpm/test_rpm_package.py
@@ -11,9 +11,13 @@ python_version = os.listdir('/usr/lib64/az/lib/')[0]
 root_dir = f'/usr/lib64/az/lib/{python_version}/site-packages/azure/cli/command_modules'
 mod_list = [mod for mod in sorted(os.listdir(root_dir)) if os.path.isdir(os.path.join(root_dir, mod)) and mod != '__pycache__']
 
-# From Fedora 36, when using `pip install --prefix`with root, the package is installed into `{prefix}/local/lib`
-# See https://docs.fedoraproject.org/en-US/fedora/latest/release-notes/developers/Development_Python/
-pytest_base_cmd = f'PYTHONPATH=/usr/lib64/az/lib/{python_version}/site-packages:/usr/lib64/az/local/lib/{python_version}/site-packages python3 -m pytest -x -v --boxed -p no:warnings --log-level=WARN'
+# From Fedora 36, use `/usr/lib64/az/local/lib/` as PYTHONPATH because installation scheme has changed.
+# `test_rpm_in_docker.sh` also creates a copy for installed az modules.
+# Ref https://docs.fedoraproject.org/en-US/fedora/latest/release-notes/developers/Development_Python/
+if os.path.exists('/usr/lib64/az/local/'):
+    pytest_base_cmd = f'PYTHONPATH=/usr/lib64/az/local/lib/{python_version}/site-packages python3 -m pytest -x -v --boxed -p no:warnings --log-level=WARN'
+else:
+    pytest_base_cmd = f'PYTHONPATH=/usr/lib64/az/lib/{python_version}/site-packages python3 -m pytest -x -v --boxed -p no:warnings --log-level=WARN'
 pytest_parallel_cmd = '{} -n auto'.format(pytest_base_cmd)
 serial_test_modules = ['botservice', 'network', 'cloud', 'appservice']
 
