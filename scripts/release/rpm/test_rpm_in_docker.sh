35dabc53cda8fe57e44bb13ae0588c849e56472d
Hang Lei <2227874+bebound@users.noreply.github.com>
2022-06-17 20:12:20 +0800

    Minor fix

--- scripts/release/rpm/test_rpm_in_docker.sh
+++ scripts/release/rpm/test_rpm_in_docker.sh
@@ -17,13 +17,17 @@ time az --version
 cd /azure-cli/
 pip3 install wheel
 ./scripts/ci/build.sh
-pip3 install pytest --prefix /usr/lib64/az
-pip3 install pytest-xdist --prefix /usr/lib64/az
 
 # From Fedora36, when using `pip install --prefix`with root, the package is installed into `{prefix}/local/lib`.
-# Making a copy of the modules to make sure tests can be added later.
+# Move installed az modules to make sure tests can be install correctly later
 # Ref https://docs.fedoraproject.org/en-US/fedora/latest/release-notes/developers/Development_Python/
-cp -r /usr/lib64/az/lib /usr/lib64/az/local/lib/
+if python3 -c "import sysconfig; print('local' in sysconfig.get_path('platlib'))" | grep True; then
+  mkdir -p /usr/lib64/az/local/
+  mv /usr/lib64/az/lib/ /usr/lib64/az/local/
+fi
+
+pip3 install pytest --prefix /usr/lib64/az
+pip3 install pytest-xdist --prefix /usr/lib64/az
 
 find /azure-cli/artifacts/build -name "azure_cli_testsdk*" | xargs pip3 install --prefix /usr/lib64/az --upgrade --ignore-installed
 find /azure-cli/artifacts/build -name "azure_cli_fulltest*" | xargs pip3 install --prefix /usr/lib64/az --upgrade --ignore-installed --no-deps
